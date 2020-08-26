acf93254242f95888dbf2787faf4934cb656b764
Juan M. Uys <opyate@gmail.com>
2020-08-26 15:18:55 +0100

    var -> const

--- src/MenuScene.ts
+++ src/MenuScene.ts
@@ -8,7 +8,7 @@ export default class MenuScene extends Phaser.Scene {
   create () {
     this.add.image(400, 300, 'space')
 
-    this.add.text(400, 200, 'Phaser 3 with Parcel\n\n< play >', {
+    this.add.text(400, 200, 'A quick maths game\n\n< play >', {
       align: 'center',
       fill: 'white',
       fontFamily: 'sans-serif',
